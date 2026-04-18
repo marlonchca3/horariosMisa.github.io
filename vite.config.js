d51e1dd9619d74a004b257967e64ff8a4483e21c
marlonchca3 <marlonchca3@gmail.com>
2026-04-18 08:57:38 -0500

    Build and deploy app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/horariosMisa.github.io/',
   plugins: [vue()],
   server: {
     port: 5173,
